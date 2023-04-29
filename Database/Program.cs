586c8a8ace4b15a836d55d062e164c9fc0c964c7
Loopylassy109 <131390896+Loopylassy109@users.noreply.github.com>
2023-04-29 14:04:14 +0100

    Update Program.cs

--- Database/Program.cs
+++ Database/Program.cs
@@ -4,20 +4,42 @@
     {
         static void Main(string[] args)
         {
+            Employee employee=new Employee("Ellen", "Admin", 23000);
             Console.WriteLine("PLease enter the employee's user ID:  ");
+           
         }
 
         class Employee
         {
-            public string name;
+            private string name;
+            private string position;
             private int age;
             private int annualsalary;
             private string email;
-            public int userID;
+            private int userID;
+
+            public Employee(string name, string position, int annualsalary) {
+                this.name = name;
+                this.position = position;
+                this.annualsalary = annualsalary;
+            }
+                
 
         }
 
+        public void setName(string newName)
+        {
+            name=newName;
+        }
 
+        public string getName()
+        {
+            return name;
+        }
+        public string getPosition(string position)
+        {
+            return position;
+        }
 
 
 
